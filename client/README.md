00ec359293bef50335232872ddadb9e1e192cddb
rwakulszowa <rwakulszowa1@gmail.com>
2020-10-23 23:25:56 +0200

    Add a simple client

---
+++ client/README.md
@@ -0,0 +1,6 @@
+# client
+Simple VueJS app.
+Doesn't do much, but shows how to dockerize a JS app, run its tests inside a container and serve a bundle.
+
+## References
+Created with [Vue CLI](cli.vuejs.org).
---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Henlo msg="Henlo from the client." />
+</template>
+
+<script>
+import Henlo from "./components/Henlo.vue";
+
+export default {
+  name: "App",
+  components: {
+    Henlo
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
